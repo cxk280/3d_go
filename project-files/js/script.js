d175d2dc1b87803949a32408ac1ccf5d3463eb0e
Christopher King <chrisking1@gmail.com>
2016-06-09 13:49:20 -0400

    added three.js library and created a black sphere on a transparent background to see how it works

--- project-files/js/script.js
+++ project-files/js/script.js
@@ -19,6 +19,29 @@ $( document ).ready(function() {
     };
   })
 
+  var scene = new THREE.Scene();
+  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  var renderer = new THREE.WebGLRenderer({alpha:true});
+  //For help designing clear renderer, referred to http://stackoverflow.com/questions/20495302/transparent-background-with-three-js
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0xffffff, 0 );
+  document.body.appendChild( renderer.domElement );
+
+  var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+  var sphere = new THREE.Mesh( geometry, material );
+  scene.add( sphere );
+
+  camera.position.z = 30;
+
+  function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+    sphere.rotation.x += 0.03;
+    sphere.rotation.y += 0.03;
+  }
+  render();
 });
 
 /*Pseudocode
